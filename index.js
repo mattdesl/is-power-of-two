c1c168084ba913be975c0e3de13aa9107a0232a5
qntm <qntm@users.noreply.github.com>
2019-01-06 19:22:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 module.exports = isPowerOfTwo
 
 function isPowerOfTwo(n) {
-  // Iterate over every possible power of two, from Number.MIN_VALUE = 2^{-1074} to 2^1023
-  // Note that thanks to the nature of 64-bit floating point arithmetic, all of these results
-  // are precisely accurate
   for (let powerOfTwo = Number.MIN_VALUE; powerOfTwo < Infinity; powerOfTwo *= 2) {
     if (n === powerOfTwo) {
       return true
